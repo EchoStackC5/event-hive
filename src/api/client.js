10569233e4a253ebfb955451fc7d8d69a3a7880c
Ayimaah <ayimaahtheodora2022@gmail.com>
2025-06-18 15:00:17 -0700

    fetched data for trending college

--- src/api/client.js
+++ src/api/client.js
@@ -1,5 +1,14 @@
 import axios from "axios";
+import { Await } from "react-router";
 
 export const apiClient = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL
 }); 
+
+export const apiFetcher = async (url) => {
+    const response = await apiClient.get(url);
+    return response.data;
+
+}
+
+export const imageBaseUrl = import.meta.env.VITE_IMAGE_BASE_URL;
--- src/components/TrendingColleges.jsx
+++ src/components/TrendingColleges.jsx
@@ -1,7 +1,31 @@
 import { Link } from "react-router";
 import CollegeCard from "./CollegeCard";
+import useSWR from "swr";
+import { apiFetcher } from "../api/client";
+import { ClipLoader } from "react-spinners";
+
+
 
 export default function TrendingColleges() {
+    // where colleges is url
+    // limit and skip enables you to load your desired number of images from
+    const { data, error, isLoading } = useSWR("/colleges?limit=3&skip=3", apiFetcher);
+    console.log(data);
+     
+    if(isLoading) {
+        return(<div className="flex justify-center items-center">
+            <ClipLoader size={100}/>
+        </div>);
+    }
+
+    if(error) {
+        return(
+        <div className="flex justify-center items-center">
+            <p className="">something went wrong</p>
+        </div>
+        );
+    }
+
     return (
         <section className="mt-20 w-[90%] mx-auto space-y-8">
             <h1 className=" text-[16px] md:text-[36px] font-medium">
@@ -9,10 +33,11 @@ export default function TrendingColleges() {
                 <span className="text-primary">colleges</span>
             </h1>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
-                {[1, 2, 3].map(n => <CollegeCard key={n} />)}
+                {data.data.map(college => <CollegeCard key={college.id}
+                    college={college} />)}
             </div>
             <div className="flex justify-center items-center">
-               <Link to= "#">
+                <Link to="#">
                     <button className="bg-primary text-white px-6 py-2 font-medium rounded-md">Load more...</button>
                 </Link>
             </div>
