fd3b4c0278f0fa836698145cc2155732b227badc
Gloria Dedo <gloriadedo100@gmail.com>
2025-06-19 05:19:26 -0700

    added create event integration

--- src/pages/CreateEvent.jsx
+++ src/pages/CreateEvent.jsx
@@ -1,21 +1,42 @@
 import Navbar from "../components/Navbar";
 import useSWR from "swr";
-import { apiFetcher } from "../api/client";
+import { apiFetcher, apiClient } from "../api/client";
+import SubmitButton from "../components/SubmitButton";
+import { useNavigate } from "react-router";
 
 export default function CreateEvent() {
-    const { data } = useSWR("/colleges", apiFetcher);
+    const navigae = useNavigate();
+    const { data, isLoading, error} = useSWR("/colleges", apiFetcher);
+    
+    const postEvent = async (data) => {
+        try {
+            const response = await apiClient.post("/events", data, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("ACCESS_TOKEN")}`
+                }
+            })
+            console.log(response.data);
+            navigae(`/view-event?id=${response.data.data.id}`);
+                
+        } catch (error) {
+            console.log(error);
+            
+        }
+
+        
+    }
     
     return (
         <>
             <Navbar />
             <section className="bg-gray-100">
                 <h1 className=" flex text-3xl font-bold items-center justify-center py-4">Create Event</h1>
-                <form action="" className="flex flex-col justify-center items-center">
+                <form action= {postEvent} className="flex flex-col justify-center items-center">
                     <div className="flex flex-col justify-around items-center">
                         <div>
                             <p className="text-xs font-bold mt-10">Select College </p>
                             <select name="college" className="h-10 w-165 bg-white text-xs rounded-md px-2">
-                                {data?.data.map((college) => (
+                                {isLoading ? <option className="animate-pulse">Loading ...</option>  : error ? <option className="text-red-500">something went wrong</option>: data.data.map((college) => (
                                     <option key={college.id} value={college.id}>
                                         {college.name}
                                     </option>
@@ -36,7 +57,7 @@ export default function CreateEvent() {
                                 name="venue"
                                 placeholder="Enter event venue" />
                         </div>
-                        <div className="flex space-x-10">
+                        {/* <div className="flex space-x-10">
                             <div>
                                 <p className="text-xs font-bold mt-10">Start time</p>
                                 <input type="time" className="h-10 w-78 bg-white text-xs rounded-md px-2"
@@ -51,19 +72,19 @@ export default function CreateEvent() {
                                     placeholder="Enter end time"
                                 />
                             </div>
-                        </div>
+                        </div> */}
                         <div className="flex space-x-10">
                             <div>
                                 <p className="text-xs font-bold mt-10">Start date</p>
-                                <input type="date" className="h-10 w-78 bg-white text-xs rounded-md px-2"
-                                    name="startDate"
+                                <input type="datetime-local" className="h-10 w-78 bg-white text-xs rounded-md px-2"
+                                    name="start"
                                     placeholder="Enter start date"
                                 />
                             </div>
                             <div>
                                 <p className="text-xs font-bold mt-10">End date</p>
-                                <input type="date" className="h-10 w-78 bg-white text-xs rounded-md px-2"
-                                    name="endDate"
+                                <input type="datetime-local" className="h-10 w-78 bg-white text-xs rounded-md px-2"
+                                    name="end"
                                     placeholder="Enter end date"
                                 />
                             </div>
@@ -74,7 +95,7 @@ export default function CreateEvent() {
                         <h1 className="font-bold text-3xl py-20 flex justify-around items-center">Event Description</h1>
                         <div className="mb-4 w-full">
                             <label className="block text-sm font-medium text-gray-700 mb-1">Event image</label>
-                            <input type="file" id="eventImageUpload" className="hidden" />
+                            <input type="file" id="eventImageUpload" name="image" className="hidden" />
                             <label
                                 htmlFor="eventImageUpload"
                                 className="cursor-pointer flex items-center justify-center w-[700px] h-[300px] px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 bg-[#F2F2F266]">
@@ -90,12 +111,26 @@ export default function CreateEvent() {
                                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm placeholder-gray-400">
                             </textarea>
                         </div>
+                        <div className="mb-4 w-full">
+                            <label className="block text-sm font-medium text-gray-700 mb-1">Event Tags</label>
+                            <select multiple name="tags" className="h-10 w-165 bg-white text-xs rounded-md px-2">
+                                <option value="social"> Social</option>
+                                <option value="eduaton"> Education</option>
+                                <option value="science-fair"> Science Fair</option>
+                                <option value="business"> Business</option>
+
+                            </select>
+                           
+                        </div>
                     </div>
+                <div className="flex justify-around items-center py-10">
+                    <SubmitButton title={"Create Event"} className="h-10 w-170 rounded-md text-white bg-primary hover:bg-blue-800"/>
+                    {/* <button className="">Create Event</button> */}
+                </div>
+                
                 </form>
 
-                <div className="flex justify-around items-center py-10">
-                    <button className="h-10 w-170 rounded-md text-white bg-primary">Create Event</button>
-                </div>
+                
             </section>
         </>
     );
