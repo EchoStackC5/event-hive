0efe99254430765644ab37777841e33d5af6a820
Abby533-alt <abigailaheto17@gmail.com>
2025-06-23 14:36:29 +0000

    integrated event details page

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router";
-export default function EventCard() {
+import { imageBaseUrl } from "../api/client";
+
+export default function EventCard({event}) {
     return (
         <div className="max-w-sm mx-auto bg-white rounded-xl shadow-md overflow-hidden">
             <div className="relative px-3 py-3 rounded-md">
                 <img
-                    src="https://plus.unsplash.com/premium_photo-1661306437817-8ab34be91e0c?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZXZlbnRzfGVufDB8fDB8fHww"
+                    src={`${imageBaseUrl}/${event.image}`} 
                     alt="Event"
                     className="w-full h-48 object-cover rounded-xl"
                 />
@@ -13,8 +15,8 @@ export default function EventCard() {
                 </span>
             </div>
             <div className="p-4 space-y-2">
-                <Link to="/view-event" className="text-base font-medium text-gray-900 leading-snug">
-                    BestSelller Book Bootcamp – write, Market & Publish Your Book – Lucknow
+                <Link to={`/view-event?id=${event.id}`} className="text-base font-medium text-gray-900 leading-snug">
+                    {event.title}
                 </Link>
                 <p className="text-sm text-violet-600 font-medium">
                     Saturday, March 18, 9.30PM
